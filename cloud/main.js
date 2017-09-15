ec7a25cacf6f0f26a34ce14600a1865439c9a908
aabdelbaqi <ahmed.abdelbaqi@gmail.com>
2017-09-15 18:58:00 +0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,13 +2,44 @@
 console.log('KAMAN Main1 Reached');
 
 
+// iOS push testing
+Parse.Cloud.define("iosPushTest", function(request, response) {
+console.log('KAMAN main2 reached');
+  // request has 2 parameters: params passed by the client and the authorized user                                                                                                                               
+  var params = request.params;
+  var user = request.user;
+
+  // Our "Message" class has a "text" key with the body of the message itself                                                                                                                                    
+  var messageText = params.text;
+
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only                                                                                                                                          
+
+  Parse.Push.send({
+    where: pushQuery, // Set our Installation query                                                                                                                                                              
+    data: {
+      alert: "Message: " + messageText
+    }
+  }, { success: function() {
+      console.log("#### PUSH OK");
+  }, error: function(error) {
+      console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true});
+
+  response.success('success');
+});
+
+
+
+
+/*
 Parse.Push.send()
 .then(function() {
   console.log('KAMAN Push sent');// Push sent!
 }, function(error) {
   console.log('KAMAN push failed');// There was a problem :(
 });
-
+*/
 
 /*arse.Push.send({
 /  where: { ... },
