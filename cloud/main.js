74b91fef37dd80114ef63bbc3d21c1d7f518f9b0
aabdelbaqi <ahmed.abdelbaqi@gmail.com>
2017-09-15 22:06:35 +0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -4,14 +4,14 @@ console.log('KAMAN Main1 Reached');
 // iOS push testing
 Parse.Cloud.define("iosPush", function(request, response) {
 
-  var user = request.user;
+  //var user = request.user;
   var params = request.params;
-  var someKey = params.someKey
+  //var someKey = params.someKey
   var data = params.data
-
-  var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only
-  pushQuery.equalTo("someKey", someKey)
+  var pushQuery=request.pushQuery;
+  //var pushQuery = new Parse.Query(Parse.Installation);
+  //pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only
+  //pushQuery.equalTo("someKey", someKey)
 
   Parse.Push.send({
     where: pushQuery, // Set our Installation query
